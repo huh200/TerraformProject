e65956c19d245135c5762f33466ce8a5b4aaf079
huh200 <132132161+huh200@users.noreply.github.com>
2025-09-27 12:30:02 +0200

    Delete Terraform Project directory

--- Terraform Project/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_name" {
-  description = "An unique S3 bucket name"
-  value       = aws_s3_bucket.web.bucket
-}
-
-output "cloudfront_url" {
-  description = "CloudFront URL (HTTPS) to access the static site"
-  value       = "https://${aws_cloudfront_distribution.web.domain_name}"
-}
